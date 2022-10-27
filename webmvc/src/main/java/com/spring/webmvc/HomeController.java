19fcb931957b4fcf53a80c654d99dfacb6a3491a
dltmdcks98 <dltmdcks98@gmail.com>
2022-10-27 11:00:50 +0900

    22.10.27 spring 이전에 했던 static 파일 적용

--- webmvc/src/main/java/com/spring/webmvc/HomeController.java
+++ webmvc/src/main/java/com/spring/webmvc/HomeController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
     }
 }
